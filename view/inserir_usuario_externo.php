090d4926297f56efb211465bfe8af8bb98b2860a
Venux115 <vag.vinicius.goulart2006@gmail.com>
2023-12-05 20:08:09 -0300

    AT-16

--- view/inserir_usuario_externo.php
+++ view/inserir_usuario_externo.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<?php require_once __DIR__ . "inicio.html"?>
 
 <div class="container" style="widith: 500px; margin-top: 30px;">
     <center>
@@ -12,3 +12,5 @@
     </div>
 
 </div>
+
+<?php require_once __DIR__ . "fim.html"?>
